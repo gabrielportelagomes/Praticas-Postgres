70e329e249d0239459e1929667698bca9c51aa05
gabrielportelagomes <gabrielgomesportela@gmail.com>
2022-12-14 21:13:33 -0300

    resolução do bônus da Prática - Exercícios Join

---
+++ Pratica - Exercicios Join/B01.sql
@@ -0,0 +1,11 @@
+SELECT * FROM roles WHERE name='Software Engineer';
+2
+
+SELECT schools.id, schools.name, courses.name AS name, companies.name AS company, roles.name AS role FROM jobs 
+JOIN applicants ON applicants."jobId"=jobs.id 
+JOIN educations ON educations."userId"=applicants."userId" 
+JOIN schools ON schools.id=educations."schoolId" 
+JOIN courses ON courses.id=educations."courseId" 
+JOIN companies ON companies.id=jobs."companyId" 
+JOIN roles ON roles.id=jobs."roleId" 
+WHERE jobs.active=true AND jobs."roleId"=2 AND jobs."companyId"=10;
